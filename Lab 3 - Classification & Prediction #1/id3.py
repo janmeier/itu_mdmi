da5db8de0cfe24f00f36a5efc90bd1e2e2b666d6
Jan Aagaard Meier <janzeh@gmail.com>
2014-03-21 14:21:31 +0100

    stuff

--- Lab 3 - Classification & Prediction #1/id3.py
+++ Lab 3 - Classification & Prediction #1/id3.py
@@ -1,14 +1,16 @@
 import sys
 import csv
 import pydot
+import random
 from math import log
 
-
 class Node(object):
+	class_counter = {}
 	def __init__(self):
-		self.attribute = '<EMPTY>' # The name of the attribute this node discriminates on, or the value of the target attribute if this is a leaf node
+		self.attribute = 'EMPTY' # The name of the attribute this node discriminates on, or the value of the target attribute if this is a leaf node
 		self.attribute_id = -1 # The index of the attribute this node discriminates on
 		self.children = [] ## A list of tuples, of val (the value of the attribute that the node discriminates on) and child, and Node, representing the nexxt decision in the tree
+		self.isLeaf = False
 
 	def toPNG(self):
 		graph = pydot.Dot(graph_type='graph')
@@ -16,10 +18,21 @@ class Node(object):
 		self.attach_childnodes(graph, None, None)
 
 		graph.write_dot('out.dot')
+		graph.write_png('out.png')
 
 
 	def attach_childnodes(self, graph, val, parent):
-		this = pydot.Node(self.attribute)
+		if not self.isLeaf:
+			if (self.attribute not in self.class_counter):
+				self.class_counter[self.attribute] = 0
+
+			Node.class_counter[self.attribute] += 1
+		else:
+			Node.class_counter[self.attribute] = ''
+
+
+		attribute = '"' + self.attribute + str(Node.class_counter[self.attribute]) + '"' #makes pydot happy
+		this = pydot.Node(attribute)
 		graph.add_node(this)
 
 		if parent != None:
@@ -28,30 +41,15 @@ class Node(object):
 		for (val, child) in self.children:
 			child.attach_childnodes(graph, val, this)
 
+	def __repr__(self):
+		return self.attribute + str(self.children)
+
 class Row(object):
 	def __init__(self, label, attributes):
-		self.label = label ## The  value of the target attribute
+		self.label = label ## The value of the target attribute
 		self.attributes = attributes ## The value of all attributes
 
-	def __repr__(self):
-		return str(self.attributes)
-
-target_idx = 0 # Target id is the index into the row of the attribute you want to classify
-rows = []
-attribute_list = [] 
-with open(sys.argv[1], 'r') as f:
-	data = csv.reader(f, delimiter=',')
-
-	headers = data.next()
-
-	i = 0
-	for header in headers:
-		if (i != target_idx):
-			attribute_list.append((i, header))
-		i = i+1
-
-	for row in data:
-		rows.append(Row(row[target_idx], row))
+	
 
 
 """
@@ -63,17 +61,17 @@ attribute_list is a list of tuples, representing the attributes that can still b
 
 Returns the root node of the decision tree
 """
-def id3(rows, attribute_list):
+def id3(rows, attribute_list, target_attribute_index):
 	n = Node()
 
-
-	split = split_by_value(rows, target_idx)
+	split = split_by_value(rows, target_attribute_index)
 	num_rows = len(rows)
 
 	label = rows[0].label
 	if (all(item.label == label for item in rows)):
 		## All rows have the same value for the target attribute. No need to recurse, so we create a leaf node with label 
 		n.attribute = label
+		n.isLeaf = True
 		return n
 	
 	if len(attribute_list) == 0:
@@ -88,12 +86,13 @@ def id3(rows, attribute_list):
 				label = val
 
 		n.attribute = val
+		n.isLeaf = True
 		return n 
 
 	# Calculate the current entropy
 	ent = entrophy(split, num_rows)
 
-	gain = 0
+	gain = -1
 	attribute_id = -1
 	attribute_name = ''
 	attribute_splits = []
@@ -109,6 +108,22 @@ def id3(rows, attribute_list):
 			attribute_name = attr_name
 			attribute_splits = attr_splits
 
+
+	if gain == 0:
+		## None of the remaninig attributes give any information gain, so we pick the majority class
+		count = 0
+		label = ''
+
+		for val in split:
+			lenght = len(split[val])
+			if lenght > count:
+				count = lenght
+				label = val
+
+		n.attribute = val
+		n.isLeaf = True
+		return n 
+
 	## Split by that
 	n.attribute = attribute_name
 	n.attribute_id = attribute_id
@@ -116,7 +131,7 @@ def id3(rows, attribute_list):
 	## Remove the attribute we are splitting by from the list
 	attribute_list = filter(lambda item: item[0] != attribute_id, attribute_list)
 	for val in attribute_splits:
-		child = id3(attribute_splits[val], attribute_list)
+		child = id3(attribute_splits[val], attribute_list, target_attribute_index)
 		n.children.append((val, child))
 
 	return n
@@ -164,6 +179,65 @@ def info_needed(splits, num_rows):
 	
 	return sum
 
+def classify(classifier, sample):
+	if len(classifier.children) > 0:
+		val = sample.attributes[classifier.attribute_id]
+		for child_val, child in classifier.children:
+			if val == child_val:
+				return classify(child, sample)
+	else:
+		return classifier.attribute
+	
 
-result = id3(rows, attribute_list)
-result.toPNG()
+rows = []
+attribute_list = [] 
+with open(sys.argv[1], 'r') as f:
+	data = csv.reader(f, delimiter=';')
+
+	headers = data.next()
+	target_idx = len(headers) - 1 ## Loves javascript is always the last attribute
+
+	i = 0
+	for header in headers:
+		if (i != target_idx):
+			attribute_list.append((i, header))
+		i = i+1
+
+	for row in data:
+		rows.append(Row(row[target_idx], row))
+
+## Shuffle rows before splitting them into training and test sets
+random.shuffle(rows)
+
+split = int(len(rows) / 5)
+training_set = rows[:split]
+test_set = rows[split:]
+
+classifier = id3(training_set, attribute_list, target_idx)
+classifier.toPNG()
+
+tp = tn = fp = fn = 0
+for test_data in test_set:
+	actual_class = test_data.label
+	predicted_class = classify(classifier, test_data)
+
+	if actual_class == 'True':
+		if predicted_class == 'True':
+			tp += 1
+		else:
+			fn += 1
+	else:
+		if predicted_class == 'True':
+			fp += 1
+		else:
+			tn += 1
+
+print "Total: ", len(rows)
+print "Training data: ", len (training_set)
+print "Test data: ", len(test_set)
+print "TP: ", tp
+print "FP: ", fp
+print "TN: ", tn
+print "FN: ", fn
+print "Precision: ", (float(tp)/(tp + fp))
+print "Recall: ", (float(tp)/(tp + fn))
